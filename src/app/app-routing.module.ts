6163a2c2c9cc4f395be699bf5ed0bbe1df2e05e6
andrea-dimarco <andr.dima99@gmail.com>
2023-08-24 17:46:36 +0200

    404 page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { EventPageComponent } from './event-page/event-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PurchaseTicketComponent } from './purchase-ticket/purchase-ticket.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path:"dashboard", component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'event-page/:id', component: EventPageComponent, runGuardsAndResolvers: 'always'},
-  { path: 'purchase-ticket', component: PurchaseTicketComponent}
+  { path: 'purchase-ticket', component: PurchaseTicketComponent},
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
