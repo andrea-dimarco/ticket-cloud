7f468c3491087f568d839f389103754a24ca47db
jemuelf98 <jemuelespiritufuentes@yahoo.it>
2023-08-30 18:56:46 +0200

    updated createTicket with new table

--- lambda/createTickets.py
+++ lambda/createTickets.py
@@ -7,34 +7,80 @@ client = boto3.client('dynamodb')
 def lambda_handler(event, context):
     body = json.loads(event['body']) 
     user_email = body['user_email']
-    n_tickets = body['n_tickets']
-    id_event = body['id_event']
-    # generate random id
-    for i in range(n_tickets):
-        # generate ticket id from timestamp and universally unique id
-        ticket_id = datetime.now().strftime('%Y%m-%d%H-%M%S-%f') + str(uuid4())
-        data = client.put_item(
-        TableName='Tickets',
-        Item={
-            'id': {
-              'S': ticket_id
+    n_tickets = int(body['n_tickets'])
+    id_event = int(body['id_event'])
+    # get available tickets 
+    data = client.get_item(
+      TableName='Events',
+      Key = {
+          "id": {
+              "N": str(id_event)
+          }        
+      }
+    )
+    available_tickets = int(data['Item']['available_tickets']['N'])
+    if (n_tickets <= available_tickets):
+      try:
+        # update available tickets 
+        update_item = client.update_item(
+            TableName='Events',
+            Key={'id': {'N': str(id_event) }},
+            UpdateExpression= "SET available_tickets = available_tickets - :decrement",
+            ExpressionAttributeValues={
+              ':decrement': {'N': str(n_tickets)}
             },
-            'event_id': {
-              'N': str(id_event)
-            },
-            'user_email': {
-              'S': user_email
-            }
+            ReturnValues='ALL_NEW'
+          )
+        
+        for i in range(n_tickets):
+          # generate ticket id from timestamp and universally unique id
+          ticket_id = datetime.now().strftime('%Y%m-%d%H-%M%S-%f') + str(uuid4())
+          data = client.put_item(
+            TableName='Tickets',
+            Item={
+                'id': {
+                  'S': str(ticket_id)
+                },
+                'event_id': {
+                  'S': str(id_event)
+                },
+                'user_email': {
+                  'S': user_email
+                }
+              }
+          )
+  
+        response = {
+          'statusCode': 200,
+          'body': 'successfully created item!',
+          'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
         }
-        )
-    
-    response = {
-      'statusCode': 200,
-      'body': 'successfully created item!',
-      'headers': {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-    }
-    
-    return response
+        
+        return response
+      except Exception as error:
+        return error
+        response = {
+        'statusCode': 200,
+        'body': 'Exception unknown!',
+        'headers': {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+          },
+        }
+      
+        return response
+        
+    else:
+      response = {
+        'statusCode': 200,
+        'body': 'Event full!',
+        'headers': {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+      }
+      
+      return response
