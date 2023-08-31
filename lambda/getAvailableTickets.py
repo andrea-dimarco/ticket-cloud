8875aabca3e8a65dd8c7cabd9451480b7e44f537
jemuelf98 <jemuelespiritufuentes@yahoo.it>
2023-08-31 11:08:23 +0200

    added lambda function

---
+++ lambda/getAvailableTickets.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+
+client = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    id = str(event["queryStringParameters"]["id"])
+    
+    query = client.get_item(
+        TableName='Events',
+        Key = {
+            "id": {
+                "N": id
+            }        
+        }
+    )
+    data = query['Item'].get('available_tickets', {}).get('N')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+        
+    }
